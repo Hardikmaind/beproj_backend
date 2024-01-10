628d1c1a527fb3b202a3dbfeeb6337f268316c60
Hardikmaind <maindhardik@gmail.com>
2024-01-10 23:29:10 +0530

    added the superuser added the models.py added admin .py all in base app

--- audio_conf/base/admin.py
+++ audio_conf/base/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+# admin.py
+from django.contrib import admin
+from .models import User, Interview, InterviewQuestion
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'is_registered', 'last_three_interviews_feedback')
+
+@admin.register(Interview)
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = ('interview_id', 'user', 'feedback')
+
+@admin.register(InterviewQuestion)
+class InterviewQuestionAdmin(admin.ModelAdmin):
+    list_display = ('question',)
--- audio_conf/base/models.py
+++ audio_conf/base/models.py
@@ -1,3 +1,34 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=255)
+    is_registered = models.BooleanField(default=False)
+    last_three_interviews_feedback = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user_name
+
+class Interview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    interview_id = models.CharField(max_length=50, unique=True)
+    feedback = models.TextField()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        # Update the last_three_interviews_feedback field after saving the interview
+        interviews = Interview.objects.filter(user=self.user).order_by('-id')[:3]
+        feedback_list = [interview.feedback for interview in interviews]
+        self.user.last_three_interviews_feedback = ', '.join(feedback_list)
+        self.user.save()
+
+    def __str__(self):
+        return f"Interview ID: {self.interview_id} - User: {self.user.user_name}"
+
+class InterviewQuestion(models.Model):
+    question = models.TextField()
+
+    def __str__(self):
+        return self.question
+
+
